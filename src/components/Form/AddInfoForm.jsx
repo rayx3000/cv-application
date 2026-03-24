565e520daa8458f0e13016863095603ae09c30ea
rayx3000 <candole2020@gmail.com>
2026-03-24 10:26:12 +0800

    add additonal info

--- src/components/Form/AddInfoForm.jsx
+++ src/components/Form/AddInfoForm.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const AddInfoForm = () => {
   return (
-    <div>AddInfoForm</div>
+    <form>
+      <p>Additional Information</p>
+      <fieldset>
+        <label htmlFor="linkedin">LinkedIn Profile:</label>
+        <input type="url" id="linkedin" name="linkedin" />
+        <label htmlFor="github">GitHub Profile:</label>
+        <input type="url" id="github" name="github" />
+        <label htmlFor="portfolio">Portfolio Website:</label>
+        <input type="url" id="portfolio" name="portfolio" />
+      </fieldset>
+    </form>
   )
 }
 
